8bbebdb86d2ae4a01b53493df4112ab6230f80fb
faxio <the_fabio@live.cl>
2023-08-17 21:58:48 -0400

    init architecture template

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+export class App {
+  PORT: number = 3001;
+  constructor(PORT: number) {
+    this.PORT = PORT;
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { App } from "./app";
+
+function main() {
+  console.log("Hello World");
+  const app = new App(2000);
+}
+main();
